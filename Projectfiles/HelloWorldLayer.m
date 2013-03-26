46ffafbde997dba7f5b6dc5dacb607c1d4f6edf2
Fredrik Carlsson <carl.fredrik.carlsson@gmail.com>
2013-03-26 16:55:26 +0100

    minor changes
    Signed-off-by: Fredrik Carlsson <carl.fredrik.carlsson@gmail.com>

--- Projectfiles/HelloWorldLayer.m
+++ Projectfiles/HelloWorldLayer.m
@@ -22,11 +22,13 @@
 	if ((self = [super init])) 
 	{
         winsize = [[CCDirector sharedDirector] winSize];
-        winsize = CGSizeMake(winsize.height, winsize.width);
+//        winsize = CGSizeMake(winsize.height, winsize.width);
         
  		CCLabelTTF* label = [CCLabelTTF labelWithString:@"Basic Spline" fontName:@"Marker Felt" fontSize:30];
+        
+//        label.position = ccp(200,200);
+//        [self addChild:label];
 		label.position = CGPointMake(0,0);
-		label.color = ccGREEN;
         CCMenuItemLabel* labItem1 = [CCMenuItemLabel itemWithLabel:label target:self selector:@selector(menuItemPressed:)];
         labItem1.position = CGPointMake(0.5*winsize.width, winsize.height-100);
         labItem1.tag = 1;
@@ -39,7 +41,7 @@
 
 -(void)menuItemPressed:(id)sender {
     
-    int tag = ((CCMenuItem*)sender).tag;
+    NSInteger tag = ((CCMenuItem*)sender).tag;
     if ( tag == 1 ) {
         NSLog(@"Pressed basic spline");
         [[CCDirector sharedDirector] replaceScene:[SimpleSplineLayer scene]];
